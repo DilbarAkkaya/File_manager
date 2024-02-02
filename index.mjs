a8b483af24aa490be01f3331146765734659e0b3
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-02-02 23:51:03 +0300

    refactor: add try catch for app function

--- index.mjs
+++ index.mjs
@@ -1,20 +1,24 @@
 import welcome from "./src/welcome.mjs";
 import readline from "node:readline/promises";
-import {stdin, stdout} from "process";
+import { stdin, stdout } from "process";
 
 const app = async () => {
-  const username = welcome();
-  console.log(`Welcome to the File Manager, ${username}!`);
-  const lineInterface = readline.createInterface({input: stdin, output: stdout});
-  lineInterface.on("SIGINT", ()=> {
-    console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
-    lineInterface.close();
-  })
-  lineInterface.on("line", (line)=>{
-    if (line === ".exit") {
-      console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  try {
+    const username = welcome();
+    console.log(`Welcome to the File Manager, ${username}!`);
+    const lineInterface = readline.createInterface({ input: stdin, output: stdout });
+    lineInterface.on("SIGINT", () => {
+      console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
       lineInterface.close();
-    }
-  });
+    })
+    lineInterface.on("line", (line) => {
+      if (line === ".exit") {
+        console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+        lineInterface.close();
+      }
+    });
+  } catch (error) {
+      console.error("Please try again, an error occured:", error)
 }
-await app()
+}
+await app();
