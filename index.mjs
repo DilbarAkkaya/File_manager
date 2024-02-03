f14ed956fd710f8e26f18a0479b9d9d7855a06ad
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-02-04 02:56:41 +0300

    fix: fix logging you are currently in..., add checking access folder for cd

--- index.mjs
+++ index.mjs
@@ -21,10 +21,10 @@ const app = async () => {
       lineInterface.close();
       //logCurrentDirectory();
     })
-    lineInterface.on("line", (line) => {
+    lineInterface.on("line", async (line) => {
       //logCurrentDirectory();
 
-      if (line.startsWith("cd")) {
+      /* if (line.startsWith("cd")) {
         const inputPath = line.slice(3).trim();
         cd(inputPath);
        // logCurrentDirectory();
@@ -36,6 +36,23 @@ const app = async () => {
       if (line === ".exit") {
         console.log(`Thank you for using File Manager, ${username}, goodbye!`);
         lineInterface.close();
+      } */
+      switch (line.split(' ')[0]) {
+        case "up":
+          up();
+          break;
+        case "cd":
+          const inputPath = line.slice(3).trim();
+         await cd(inputPath);
+         // logCurrentDirectory();
+          break;
+        case ".exit":
+          console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+          lineInterface.close();
+          break;
+        default:
+          console.log("Invalid input. Please enter a valid command.");
+          break;
       }
       lineInterface.setPrompt('\x1b[95m>> \x1b[0m');
       logCurrentDirectory();
@@ -45,7 +62,7 @@ const app = async () => {
       logCurrentDirectory();
     });
   } catch (error) {
-    console.error("Please try again, an error occured:", error);
+    console.error("Operation failed:", error);
     logCurrentDirectory();
   }
 }
