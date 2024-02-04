94842875502e416441cee5af711d0e25fbbcf652
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-02-04 12:17:11 +0300

    feat: implement files and folders sort, and log them in table

--- src/ls.mjs
+++ src/ls.mjs
@@ -1,15 +1,26 @@
 import fs from 'fs/promises';
 import path from 'path';
 const ls = async () => {
-    const arrayOfNames = [];
+    //const arrayOfNames = [];
+    const arrayOfFiles = [];
+    const arrayOfFolders = [];
     try {
         const files = await fs.readdir(process.cwd(), { withFileTypes: true });
         if (files.length == 0) { throw new Error(`Operation failed`) }
         else {
             for (let file of files) {
-              arrayOfNames.push(file['name']);
+              console.log(file)
+              const stats = await fs.stat(path.join((process.cwd(), file.name)));
+              if (stats.isDirectory()) {
+                arrayOfFolders.push(file.name)
+              } else if (stats.isFile()) {
+                arrayOfFiles.push(file.name)
+              }
             }
-            console.log(arrayOfNames);
+            arrayOfFolders.sort();
+            arrayOfFiles.sort();
+            const data = [...arrayOfFolders, ...arrayOfFiles]
+            console.table(data);
         }
     }
     catch (err) {
