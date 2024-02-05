6f323afdd6f7a597e31932ceb61859446b2bd9ea
DilbarAkkaya <dilbarmutavalova@gmail.com>
2024-02-06 02:44:05 +0300

    fix: fix exit ctrl+c commands

--- index.mjs
+++ index.mjs
@@ -28,102 +28,99 @@ const app = async () => {
 
     lineInterface.on("SIGINT", () => {
       console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
+      logCurrentDirectory();
       lineInterface.close();
     })
 
     lineInterface.on("line", async (line) => {
-      const partsOfLine = line.split(' ');
-      switch (line.split(' ')[0]) {
-        case "up":
-          up();
-          break;
-        case "cd":
-          const inputPath = line.slice(3).trim();
-          await cd(inputPath);
-          break;
-        case "ls":
-          try {
-            await ls();
-          } catch (error) {
-            console.error(`Operation failed: ${error}`);
-          }
-          break;
-        case "cat":
-          const input = line.slice(4).trim();
-          try {
-            await cat(input);
-          } catch (error) {
-            console.error(`Operation failed: ${error}`);
-          }
-          break;
-        case "add":
-          const fileName = line.slice(4).trim();
-          try {
-            await add(fileName);
-          } catch (error) {
-            console.error(`Operation failed: ${error}`);
-          }
-          break;
-        case "rn":
-          const arrOfArgs = line.split(' ');
-          const newFileName = arrOfArgs[2];
-          try {
-            await rename(arrOfArgs[1], newFileName);
-          } catch (error) {
-            console.error(`Operation failed: ${error}`);
-          }
-          break;
-        case "cp":
-          try {
-            await copy(partsOfLine[1], partsOfLine[2]);
-          } catch (error) {
-            console.error(`Operation failed: ${error}`);
-          }
-          break;
-        case "mv":
-          try {
-            await move(partsOfLine[1], partsOfLine[2]);
-          } catch (error) {
-            console.error(`Operation failed: ${error}`);
-          }
-          break;
-        case "rm":
-          const fileToDelete = line.slice(3).trim();
-          try {
-            await remove(fileToDelete);
-          } catch (error) {
-            console.error(`Operation failed: ${error}`);
-          }
-          break;
-        case "os":
-          getInfoFromOS(partsOfLine[1])
-          break;
-        case "hash":
-          await calculateHash(partsOfLine[1])
-          break;
-        case "compress":
-          await compress(partsOfLine[1], partsOfLine[2])
-          break;
-        case "decompress":
-          await decompress(partsOfLine[1], partsOfLine[2])
-          break;
-        case ".exit":
-          console.log(`Thank you for using File Manager, ${username}, goodbye!`);
-          lineInterface.close();
-          break;
-        default:
-          console.log("Invalid input. Please enter a valid command.");
-          break;
+      if (line === '.exit') {
+        console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+        lineInterface.close();
+      } else {
+        const partsOfLine = line.split(' ');
+        switch (line.split(' ')[0]) {
+          case "up":
+            up();
+            break;
+          case "cd":
+            const inputPath = line.slice(3).trim();
+            await cd(inputPath);
+            break;
+          case "ls":
+            try {
+              await ls();
+            } catch (error) {
+              console.error(`Operation failed: ${error}`);
+            }
+            break;
+          case "cat":
+            const input = line.slice(4).trim();
+            try {
+              await cat(input);
+            } catch (error) {
+              console.error(`Operation failed: ${error}`);
+            }
+            break;
+          case "add":
+            const fileName = line.slice(4).trim();
+            try {
+              await add(fileName);
+            } catch (error) {
+              console.error(`Operation failed: ${error}`);
+            }
+            break;
+          case "rn":
+            const arrOfArgs = line.split(' ');
+            const newFileName = arrOfArgs[2];
+            try {
+              await rename(arrOfArgs[1], newFileName);
+            } catch (error) {
+              console.error(`Operation failed: ${error}`);
+            }
+            break;
+          case "cp":
+            try {
+              await copy(partsOfLine[1], partsOfLine[2]);
+            } catch (error) {
+              console.error(`Operation failed: ${error}`);
+            }
+            break;
+          case "mv":
+            try {
+              await move(partsOfLine[1], partsOfLine[2]);
+            } catch (error) {
+              console.error(`Operation failed: ${error}`);
+            }
+            break;
+          case "rm":
+            const fileToDelete = line.slice(3).trim();
+            try {
+              await remove(fileToDelete);
+            } catch (error) {
+              console.error(`Operation failed: ${error}`);
+            }
+            break;
+          case "os":
+            getInfoFromOS(partsOfLine[1])
+            break;
+          case "hash":
+            await calculateHash(partsOfLine[1])
+            break;
+          case "compress":
+            await compress(partsOfLine[1], partsOfLine[2])
+            break;
+          case "decompress":
+            await decompress(partsOfLine[1], partsOfLine[2])
+            break;
+          default:
+            console.log("Invalid input. Please enter a valid command.");
+            break;
+        }
       }
-      lineInterface.setPrompt('\x1b[95m>> \x1b[0m');
+    //  lineInterface.setPrompt('\x1b[95m>> \x1b[0m');
       logCurrentDirectory();
-      lineInterface.prompt();
+ //     lineInterface.prompt();
     });
-
-    lineInterface.on("close", () => {
-      logCurrentDirectory();
-    });
-
   } catch (error) {
     console.error(`Operation failed: ${err}`);
     logCurrentDirectory();
